d33774b4096032f3a77cf0d1925b4e8a9fcf13f4
Tec247 <pegazoz99@gmail.com>
2021-10-15 16:30:46 -0300

    Coreção do .env

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,16 +1,22 @@
 // import {initializeApp} from 'firebase/app';
 
 import firebase from 'firebase/compat/app';
-
-
 import 'firebase/compat/auth';
 import 'firebase/compat/database';
 
-const firebaseConfig = {
+const firebaseConfig = {    
+  // apiKey: "AIzaSyDwJ-rzdjKV0JlvSvh9oSc142MtjMvk52c",
+  // authDomain: "letmeask-32d21.firebaseapp.com",
+  // databaseURL: "https://letmeask-32d21-default-rtdb.firebaseio.com",
+  // projectId: "letmeask-32d21",
+  // storageBucket: "letmeask-32d21.appspot.com",
+  // messagingSenderId: "540563329675",
+  // appId: "1:540563329675:web:0f26e039e87fe0b73d3a8e"
+
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
+    projectId: process.env.REACT_APP_REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID
@@ -24,5 +30,3 @@ const firebaseConfig = {
   export {firebase, auth, database}
 
   // initializeApp(firebaseConfig);
-  //teste
-  
