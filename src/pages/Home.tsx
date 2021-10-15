c27b661f751a8dfc113dbc73eb16742e3e9c1a11
Tec247 <pegazoz99@gmail.com>
2021-10-15 12:38:01 -0300

    Autetificação via Google

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,43 +1,51 @@
-import illustrationImg from '../asserts/images/illustration.svg';
-import logoImg from '../asserts/images/logo.svg';
-import googleIconImg from '../asserts/images/google-icon.svg';
+import { useHistory } from "react-router-dom";
+import { auth, firebase } from "../services/firebase";
+import illustrationImg from "../asserts/images/illustration.svg";
+import logoImg from "../asserts/images/logo.svg";
+import googleIconImg from "../asserts/images/google-icon.svg";
 
-import '../styles/auth.scss';
-import { Button } from '../components/Button';
+import "../styles/auth.scss";
+import { Button } from "../components/Button";
 
-export function Home(){
-    return(
-        <div id='page-auth'>
-            <aside>
-                <img src={illustrationImg} alt="ilustração simbolizando perguntas e respostas" />
-                <strong>Crie salas de Q&amp;A ao-vivo</strong>
-                <p>Tire as dúvidas da sua audiência em tempo-real</p>
-            </aside>
+export function Home() {
+  const history = useHistory();
 
-            <main>
-                <div className="main-content">
-                    <img src= {logoImg} alt="Letmeask" />
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
 
-                    <button className="create-room">
-                        <img src={googleIconImg} alt="Logo do Google" />
-                        Crie sua sala com o Google
-                        </button>
-                        <div className="separator">
-                            ou entre em uma sala
-                        </div>
-                        <form>
-                            <input
-                             type="text"
-                             placeholder='Digite o código da sala'
-                            />
-                            <Button type = "submit">
-                                 Entrar na sala
-                            </Button>
-                        </form>
-                </div>
-            </main>
+    auth.signInWithPopup(provider).then((result) =>{
+      console.log(result);
+    });
+
+     history.push("/rooms/new");
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img
+          src={illustrationImg}
+          alt="ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+
+          <button onClick={handleCreateRoom} className="create-room">
+            <img src={googleIconImg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+          <div className="separator">ou entre em uma sala</div>
+          <form>
+            <input type="text" placeholder="Digite o código da sala" />
+            <Button type="submit">Entrar na sala</Button>
+          </form>
         </div>
-    )
-
+      </main>
+    </div>
+  );
 }
-
