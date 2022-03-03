c0406762d4eef07e02f304052839e43f3fd45212
JeanTec247 <pegazoz99@gmail.com>
2022-03-03 10:08:56 -0300

    Router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+// import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom"
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from "./pages/Room";
@@ -19,5 +20,5 @@ function App(): JSX.Element {
     </BrowserRouter>
   );
 }
-
+//
 export default App;
